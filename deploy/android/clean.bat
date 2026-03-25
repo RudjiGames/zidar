5b7cd60c1ba9a9c789497134276b0a9912e9631a
Milos Tosic <milos@rudji.com>
2026-03-25 11:50:26 +0100

    Delete build files

--- deploy/android/clean.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rmdir bin /s /q
-rmdir libs /s /q
-rmdir obj /s /q
-del local.properties
-del proguard-project.txt
-del project.properties
--- deploy/android/jni/Application.mk
+++
@@ -1,8 +0,0 @@
-#
-# Copyright 2013-2015 Milos Tosic. All rights reserved.
-# License: http://www.opensource.org/licenses/BSD-2-Clause
-#
-
-APP_ABI := @@ARCH@@
-APP_PLATFORM := android-@@ANDROID_VER@@
-
